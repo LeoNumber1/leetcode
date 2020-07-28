ca7c7855316fcebb5f4bf5604db1ba78353fab00
liyuan <liyuan@wondersgroup.com>
2020-07-28 15:20:03 +0800

    9回文数官方解答

--- 9回文数/9回文数.go
+++ 9回文数/9回文数.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	x := 10
 	//x = 121
+	x = 1231891321
 	fmt.Println(isPalindrome(x))
+	fmt.Println(isPalindromeString(x))
+	fmt.Println(isPalindromeOfficial1(x))
 }
 
 func isPalindrome(x int) bool {
-	if x < 0 {
+	if x < 0 || (x%10 == 0 && x != 0) {
 		return false
 	}
 
@@ -23,4 +29,30 @@ func isPalindrome(x int) bool {
 		res += y
 	}
 	return res == xBefore
+}
+
+func isPalindromeString(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	xStr := strconv.Itoa(x)
+	n := len(xStr)
+	for i := 0; i < n/2; i++ {
+		if xStr[i] != xStr[n-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func isPalindromeOfficial1(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	var revertedNumber int
+	for revertedNumber < x {
+		revertedNumber = revertedNumber*10 + x%10
+		x /= 10
+	}
+	return revertedNumber == x || revertedNumber/10 == x
 }
