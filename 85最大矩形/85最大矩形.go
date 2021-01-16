bae4e4a1749268f4bcbd87a4fcc85e28fb9d06bc
liyuan <liyuan@wondersgroup.com>
2021-01-16 15:13:41 +0800

    803 打砖块

--- 85最大矩形/85最大矩形.go
+++ 85最大矩形/85最大矩形.go
@@ -10,10 +10,11 @@ func main() {
 	//matrix = [][]byte{{'1', '0'}, {'1', '0'}}
 	//matrix = [][]byte{{'0', '1'}, {'0', '1'}}
 
-	//fmt.Println(maximalRectangle(matrix))
-	fmt.Println(maximalRectangleOfficial(matrix))
+	fmt.Println(maximalRectangle(matrix))
+	//fmt.Println(maximalRectangleOfficial2(matrix))
 }
 
+//4 ms	4 MB
 func maximalRectangle(matrix [][]byte) int {
 	m := len(matrix)
 	if m == 0 {
@@ -67,6 +68,7 @@ func maximalRectangle(matrix [][]byte) int {
 	return ans
 }
 
+//4 ms	4 MB
 func maximalRectangleOfficial(matrix [][]byte) (ans int) {
 	if len(matrix) == 0 {
 		return
@@ -94,6 +96,9 @@ func maximalRectangleOfficial(matrix [][]byte) (ans int) {
 			width := left[i][j]
 			area := width
 			for k := i - 1; k >= 0; k-- {
+				if left[k][j] == 0 {
+					break
+				}
 				width = min(width, left[k][j])
 				area = max(area, (i-k+1)*width)
 			}
@@ -115,4 +120,57 @@ func max(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func maximalRectangleOfficial2(matrix [][]byte) (ans int) {
+	if len(matrix) == 0 {
+		return
+	}
+	m, n := len(matrix), len(matrix[0])
+	left := make([][]int, m)
+	for i, row := range matrix {
+		left[i] = make([]int, n)
+		for j, v := range row {
+			if v == '0' {
+				continue
+			}
+			if j == 0 {
+				left[i][j] = 1
+			} else {
+				left[i][j] = left[i][j-1] + 1
+			}
+		}
+	}
+	for j := 0; j < n; j++ { // 对于每一列，使用基于柱状图的方法
+		up := make([]int, m)
+		down := make([]int, m)
+		stk := []int{}
+		for i, l := range left {
+			for len(stk) > 0 && left[stk[len(stk)-1]][j] >= l[j] {
+				stk = stk[:len(stk)-1]
+			}
+			up[i] = -1
+			if len(stk) > 0 {
+				up[i] = stk[len(stk)-1]
+			}
+			stk = append(stk, i)
+		}
+		stk = nil
+		for i := m - 1; i >= 0; i-- {
+			for len(stk) > 0 && left[stk[len(stk)-1]][j] >= left[i][j] {
+				stk = stk[:len(stk)-1]
+			}
+			down[i] = m
+			if len(stk) > 0 {
+				down[i] = stk[len(stk)-1]
+			}
+			stk = append(stk, i)
+		}
+		for i, l := range left {
+			height := down[i] - up[i] - 1
+			area := height * l[j]
+			ans = max(ans, area)
+		}
+	}
+	return
 }
